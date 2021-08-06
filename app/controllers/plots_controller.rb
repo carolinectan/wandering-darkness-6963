7a20bbd8d249eceae0153c3855321a412bef80f1
Caroline Tan <carolinectan3@gmail.com>
2021-08-06 10:51:15 -0400

    Add plot index route, view, and action

---
+++ app/controllers/plots_controller.rb
@@ -0,0 +1,27 @@
+class PlotsController < ApplicationController
+  def index
+    @plots = Plot.all
+  end
+  # def show
+  #
+  # end
+  # def new
+  #
+  # end
+  # def create
+  #
+  # end
+  # def edit
+  #
+  # end
+  # def update
+  #
+  # end
+  # def destroy
+  #
+  # end
+  # private
+  # def _params
+  #   params.permit(:)
+  # end
+end
