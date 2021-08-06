8bd329f4824a2ba72599e3425e91f02b4c4d3637
Caroline Tan <carolinectan3@gmail.com>
2021-08-06 10:33:13 -0400

    Add model relationships; set up migrations

--- spec/models/plant_spec.rb
+++ spec/models/plant_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Plant, type: :model do
   describe 'relationships' do
-    it { should have_many(:plant_pots) }
-    it { should have_many(:plots).through(:plant_pots) }
+    it { should have_many(:plant_plots) }
+    it { should have_many(:plots).through(:plant_plots) }
   end
 
   # describe 'validations' do
--- spec/models/plot_spec.rb
+++ spec/models/plot_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe Plot do
   describe 'relationships' do
     it { should belong_to(:garden) }
-    it { should have_many(:plant_pots) }
-    it { should have_many(:plants).through(:plant_pots) }
+    it { should have_many(:plant_plots) }
+    it { should have_many(:plants).through(:plant_plots) }
 
   end
 end
